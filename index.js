f38314b0836b34dfe31cd0960382ddbdcbbf0d63
Michael Bowers <mike.c.bowers@gmail.com>
2021-06-16 08:35:34 -0400

    Minor tweaks.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const server = require('./server');
 const port = process.env.PORT || 3000;
 
+console.log(`Environment: ${JSON.stringify(process.env)}`);
+
 server.listen(port);
 console.log(`Server listening on port ${port}`);
 
